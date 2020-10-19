f33891073d850dda83066e86842833f9a521a83e
roojay <roojay520@gmail.com>
2020-10-19 17:50:22 +0800

    chore: 更新 webpack 配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
 
 const mode = process.env.NODE_ENV || 'production';
 
@@ -7,17 +6,11 @@ module.exports = {
   mode,
   target: 'webworker',
   entry: {
-    'dist/worker': './src/index.ts',
     'worker/script': './src/index.ts',
   },
   output: {
     path: path.resolve(__dirname, './'),
     filename: '[name].js',
-  },
-  devtool: 'cheap-source-map',
-  optimization: {
-    minimize: false,
-    minimizer: [new TerserPlugin({ include: /script/ })],
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
