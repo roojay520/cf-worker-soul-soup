a42769ddb57564bee875828e036dcc3adf091f5f
roojay <roojay520@gmail.com>
2021-03-16 10:21:04 +0800

    refactor: 优化根据 id 取值

--- src/router.ts
+++ src/router.ts
@@ -1,23 +1,19 @@
 import { Router } from 'itty-router';
 import sourceData from './data';
 
-type Obj = {
-  [propName: string]: string;
-};
-
-interface Req extends Request {
-  params?: Obj;
-  query?: Obj;
+function getRandom(start: number, end: number) {
+  var num = Math.floor(Math.random() * (end - start + 1) + start);
+  return num;
 }
 
 const getData = (id?: number | string) => {
   let index = id;
-  if (index) index = +index + 1;
+  if (index) index = Number(id);
   const maxLen = sourceData.length;
-  if (!Number.isInteger(index) || (index as number) > maxLen) {
-    index = Math.floor(Math.random() * maxLen);
+  if (!Number.isInteger(index) || (index as number) >= maxLen || (index as number) < 1) {
+    index = getRandom(1, maxLen);
   }
-  const data = { id: index, title: sourceData[index as number] };
+  const data = { id: index, title: sourceData[(index as number) - 1] };
 
   return new Response(JSON.stringify(data));
 };
@@ -25,7 +21,7 @@ const getData = (id?: number | string) => {
 async function handleRequest(request: Request) {
   const router = Router();
 
-  router.get('/:id', async (req: Req) => getData(req.params?.id)).get('*', () => getData());
+  router.get('/:id', async (req) => getData(req.params?.id)).get('*', () => getData());
 
   return router.handle(request);
 }
